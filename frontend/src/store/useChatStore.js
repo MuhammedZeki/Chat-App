3ab36a4d921bdd105b7f404a0a06485b4f4d648b
MuhammedZeki <muhammedzekiylmz5353@gmail.com>
2025-05-11 20:12:00 +0300

    change

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -8,7 +8,6 @@ export const useChatStore = create((set, get) => ({
   users: [],
   isUsersLoading: false,
   isMessagesLoading: false,
-  user: null,
   getUsers: async () => {
     set({ isUsersLoading: true });
     try {
