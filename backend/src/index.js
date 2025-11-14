388fc1944b6f0ab929f3d3557a7fcb7e7aed7504
MuhammedZeki <muhammedzekiylmz5353@gmail.com>
2025-11-14 12:45:21 +0300

    fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,9 +11,11 @@ const path = require("path");
 config();
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
