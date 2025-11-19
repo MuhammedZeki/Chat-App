5fdd71beb41c1a221b45e3509759bb07bf3cd50c
MuhammedZeki <muhammedzekiylmz5353@gmail.com>
2025-11-20 01:06:37 +0300

    fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,10 +23,10 @@ app.use("/auth", AuthRoutes);
 app.use("/messages", MessageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
 
